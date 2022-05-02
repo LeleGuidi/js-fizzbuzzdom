b90beab1858de6483b29a3b0490addb7620167bf
LeleGuidi <emanuele.guidi98@outlook.it>
2022-05-02 15:23:55 +0200

    Milestone 3 completato

--- js/main.js
+++ js/main.js
@@ -1,26 +1,23 @@
-// MILESTONE 2
-// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
 for (let i = 1; i <= 100; i++) {
     const element = i;
     const div = document.createElement(`div`);
     const container = document.querySelector(`div.container`)
-    div.classList.add(`box`);
 
     if (element % 3 == 0 && element % 5 == 0) {
         div.append(`FizzBuzz`)
+        div.classList.add(`box`, `bg_red`)
         container.append(div)
     } else if (element % 3 == 0) {
         div.append(`Fizz`)
+        div.classList.add(`box`, `bg_green`)
         container.append(div)
     } else if (element % 5 == 0) {
         div.append(`Buzz`)
+        div.classList.add(`box`, `bg_yellow`)
         container.append(div)
     } else {
         div.append(element)
+        div.classList.add(`box`, `bg_blu`)
         container.append(div)
     }
 }
-
-
-// MILESTONE 3
-// Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
