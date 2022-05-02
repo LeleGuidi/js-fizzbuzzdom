17f5524ade78e01cc93b2889fc4fd9bc74d6a6ba
LeleGuidi <emanuele.guidi98@outlook.it>
2022-05-02 14:57:34 +0200

    Milestone 1 completato

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 // Scrivere un programma che stampi in console i numeri da 1 a 100.
+for (let i = 1; i <= 100; i++) {
+    const element = i
 // MILESTONE 1
 // Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+    if (element % 3 == 0 && element % 5 == 0) {
+        console.log(`FizzBuzz`)
+    } else if (element % 3 == 0) {
+        console.log(`Fizz`)
+    } else if (element % 5 == 0) {
+        console.log(`Buzz`)
+    } else {
+        console.log(element)
+    }
+}
 // MILESTONE 2
 // Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
 // MILESTONE 3
